38303e639ebd1538f84faff5263e76d10d458d8c
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2022-12-07 00:12:13 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import "./app.css";
 import { ApplicationProvider } from "./context";
 
 import { Route, Switch } from "react-router-dom";
+import DetailPage from "./pages/DetailPage/DetailPage";
+import { useEffect, useState } from "react";
+import { customers, reports } from "./data";
 
 function App() {
+	const [allCustomers, setAllCustomers] = useState([]);
+	const [allReports, setAllReports] = useState([]);
+
+	useEffect(() => {
+		setAllCustomers(customers);
+		setAllReports(reports);
+	}, []);
+
 	return (
-		<ApplicationProvider>
+		<ApplicationProvider
+			value={{ allCustomers, allReports, setAllReports }}
+		>
 			<Switch>
 				<Route exact path="/">
 					<div className="App">
 						<header className="App-header">Header</header>
 					</div>
 				</Route>
+
+				<Route
+					path="/details/:id"
+					render={(routerObject) => (
+						<DetailPage id={routerObject.match.params.id} />
+					)}
+				/>
 			</Switch>
 		</ApplicationProvider>
 	);
