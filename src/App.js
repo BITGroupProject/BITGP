34e67c22f76d84134bc7971bbfa435c81847c328
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2022-12-06 11:16:39 +0100

    Initial project configuration and vendor istalation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./app.css";
+import { ApplicationProvider } from "./context";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Header</header>
-    </div>
-  );
+	return (
+		<ApplicationProvider>
+			<Switch>
+				<Route exact path="/">
+					<div className="App">
+						<header className="App-header">Header</header>
+					</div>
+				</Route>
+			</Switch>
+		</ApplicationProvider>
+	);
 }
 
 export default App;
