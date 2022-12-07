0d4780ac7173e59ae4a16326361b07e183fb80b0
vlastimir-bulatovic <bulatovic.vlastimir98@gmail.com>
2022-12-07 12:35:08 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -4,45 +4,47 @@ import HomePage from "./pages/HomePage/HomePage";
 import { Route, Switch } from "react-router-dom";
 import AllReports from "./pages/AllReports/AllReports";
 import Wizard from "./pages/Wizard/Wizard";
-import "./App.css";
+import "./app.css";
 
 import DetailPage from "./pages/DetailPage/DetailPage";
 import { useEffect, useState } from "react";
 import { candidates, reports } from "./data";
 
 function App() {
-  const [allCandidates, setAllCandidates] = useState([]);
-  const [allReports, setAllReports] = useState([]);
+	const [allCandidates, setAllCandidates] = useState([]);
+	const [allReports, setAllReports] = useState([]);
 
-  useEffect(() => {
-    setAllCandidates(candidates);
-    setAllReports(reports);
-  }, []);
+	useEffect(() => {
+		setAllCandidates(candidates);
+		setAllReports(reports);
+	}, []);
 
-  return (
-    <ApplicationProvider value={{ allCandidates, allReports, setAllReports }}>
-      <Switch>
-        <Route exact path="/home">
-          <HomePage />
-        </Route>
+	return (
+		<ApplicationProvider
+			value={{ allCandidates, allReports, setAllReports }}
+		>
+			<Switch>
+				<Route exact path="/home">
+					<HomePage />
+				</Route>
 
-        <Route path="/reports">
-          <AllReports />
-        </Route>
+				<Route path="/reports">
+					<AllReports />
+				</Route>
 
-        <Route path="/wizard">
-          <Wizard />
-        </Route>
+				<Route path="/wizard">
+					<Wizard />
+				</Route>
 
-        <Route
-          path="/details/:id"
-          render={(routerObject) => (
-            <DetailPage id={routerObject.match.params.id} />
-          )}
-        />
-      </Switch>
-    </ApplicationProvider>
-  );
+				<Route
+					path="/details/:id"
+					render={(routerObject) => (
+						<DetailPage id={routerObject.match.params.id} />
+					)}
+				/>
+			</Switch>
+		</ApplicationProvider>
+	);
 }
 
 export default App;
