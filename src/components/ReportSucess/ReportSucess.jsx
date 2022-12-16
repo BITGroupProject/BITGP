2ef0b5a37ce912bc3a3561ec58d732192374cf68
mikash91 <mihailo_jesic@yahoo.com>
2022-12-16 16:32:34 +0100

    Wizard success tweaks

--- src/components/ReportSucess/ReportSucess.jsx
+++ src/components/ReportSucess/ReportSucess.jsx
@@ -4,50 +4,50 @@ import { applicationContext } from "../../context";
 import "./reportSuccess.css";
 
 function ReportSucess(props) {
-	const { token } = useContext(applicationContext);
+  const { token } = useContext(applicationContext);
 
-	useEffect(() => {
-		let entry = {
-			id: 10000000 + Math.round(Math.random() * 89999999),
-			candidateId: props.candidate.candidateId,
-			candidateName: props.candidate.candidateName,
-			companyId: props.company.companyId,
-			companyName: props.company.companyName,
-			interviewDate: props.interviewDate,
-			phase: props.phase,
-			status: props.status,
-			note: props.note,
-		};
+  useEffect(() => {
+    let entry = {
+      id: 10000000 + Math.round(Math.random() * 89999999),
+      candidateId: props.candidate.candidateId,
+      candidateName: props.candidate.candidateName,
+      companyId: props.company.companyId,
+      companyName: props.company.companyName,
+      interviewDate: props.interviewDate,
+      phase: props.phase,
+      status: props.status,
+      note: props.note,
+    };
 
-		fetch("https://node-api-krmk.onrender.com/api/reports", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + token,
-			},
-			body: JSON.stringify(entry),
-		})
-			.then((response) => response.json())
-			.then((entry) => {
-				console.log("Success:", entry);
-			})
-			.catch((error) => {
-				console.error("Error:", error);
-			});
-	}, []);
+    fetch("https://node-api-krmk.onrender.com/api/reports", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(entry),
+    })
+      .then((response) => response.json())
+      .then((entry) => {
+        console.log("Success:", entry);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-	return (
-		<>
-			<div className="report-success-container">
-				<div className="report-success-box">
-					<h2 className="report-success-message">Report Created!</h2>
-				</div>
-				<Link className="link--back" to="/reports">
-					Back
-				</Link>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="report-success-container">
+        <div className="report-success-box">
+          <h2 className="report-success-message">Report Created!</h2>
+        </div>
+        <Link className="link--back" to="/reports">
+          Reports
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default ReportSucess;
